d35fafc947a9c23d17615119653a049003b97727
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2026-04-15 21:54:55 +0200

    content: add offer content

--- src/routes/offer.tsx
+++ src/routes/offer.tsx
@@ -41,13 +41,14 @@ export default function Offer() {
             <img src="img/loop3.JPG" alt="Loopstation Projekt" />
           </div>
 
-          <h2>Flexibel, spontan mit dem Sound einer Band</h2>
+          <h2>Flexibel & spontan - mit dem Sound einer ganzen Band</h2>
           <p>Da ich hier alleine unterwegs bin, kann ich sehr flexibel auf verschiedene Situationen reagieren und spontan auf die Bedürfnisse der Zuschauer eingehen. <br />
             Auch die Absprache und Buchung ist unkompliziert, zuverlässig und schnell abgewickelt. </p>
           <p>Ich bin nicht abhängig von externen Dienstleistern und habe bei Bedarf meine eigene Tontechnik dabei.</p>
 
           <h4>Events, Messen, Hochzeiten..</h4>
-          <p>Dieses Projekt eignet sich hervorragend für verschiedene Anlässe und Veranstaltungen. Überall wo es luftige, lockere Beats mit jazzigem Saxophon und einer ordentlicheen Portion Soul benötigt..
+          <p>Dieses Projekt eignet sich hervorragend für verschiedene Anlässe und Veranstaltungen.
+            <br />Überall wo es luftige, lockere Beats mit jazzigem Saxophon und einer ordentlicheen Portion Soul benötigt..
           </p>
 
 
@@ -64,17 +65,35 @@ export default function Offer() {
 
         <span class="seperator"></span>
         <h3>Elektro &  Sax</h3>
-        <p>Ich produziere seit meiner Schulzeit elektronische Musik, die ich auch auflege und dazu Saxophon spiele. <br />Ich lege hierbei nur sehr ausgewählte Sounds auf und bin nicht als DJ in dieser Form unterwegs. </p>
+        <p>Ich produziere seit meiner Schulzeit elektronische Musik, die ich auch auflege und dazu Saxophon spiele. <br />Ich lege hierbei nur sehr ausgewählte Sounds auf und bin nicht als DJ in dieser Form unterwegs. <br />
+          Stilistisch handelt es sich hier um Deep-House, Melodic-House, Down Tempo mit Saxophon on Top.</p>
+
+        <div class="gallery-container">
+          <img src="img/walk.JPG" alt="Elektro &  Sax Projekt" />
+          <img src="img/elektro2.JPG" alt="Elektro &  Sax Projekt" />
+        </div>
+
         <h4>Rooftop Bars, Afterwork, Open Air Events..</h4>
         <p>Wer liebt es nicht? <br />
-        Ein kühles Getränk, schönes Wetter und ein triebende eletronische Beats mit mal Funky, mal sphärischen Saxophon Sounds.</p>
+          Ein kühles Getränk, schönes Wetter und treibende eletronische Beats mit Saxophon Sounds. <br /> So stelle zumindest Ich mir ein schönes Warming Up von einem erfolgreichen Event vor.</p>
 
-        <p>Meistens spiele ich aber zusammen mit meinem Kollegen und langjährigen Freund <a class="link" href="https://www.bikebalance.de/">Christian Gehrmann</a> <br />  Urbane Elektro Sounds, gepaart mit Funky Saxophon Riffs!</p>
-        <p>Wir spielen auf Rooftop Bars, Outdoor Events und überall sonst wo es eine lockere urbane Atmosphäre braucht!</p>
+        <p>Häufig spiele ich aber zusammen mit meinem Kollegen und langjährigen Freund und DJ <a class="link" href="https://www.bikebalance.de/">Christian Gehrmann</a> <br />  Urbane Elektro Sounds, gepaart mit Funky Saxophon Riffs!</p>
+        Wir spielen zusammen in Rooftop Bars, auf Outdoor Events und überall sonst wo es eine lockere urbane Atmosphäre braucht, <br />die später in einem amtlichen DJ Set mündet. <br />
+        
+        <div class="gallery-container">
+          <img style={"height:350px;"} src="img/elektro1.JPG" alt="Elektro &  Sax Projekt" />
+        </div>
+
+
         <span class="seperator"></span>
+
+
         <h3 id="arrangements" >Arrangements</h3>
         Ich schreibe seit vielen Jahren Arrangements für Bläser, Orchester und Big Band. <br />
         Seit 2017 bin ich als Dirigent tätig und habe für verscheidenste Formationen schon etliche Arrangements geschrieben<br />
+        <h4>Rock & Pop Medleys | Solo Stücke | Orchester Arrangements mit Gesang</h4>
+        
+
         <p>Es macht mir sehr viel Spaß die Arrangements die ich schreibe, auch aufzuführen und mit den Menschen einzustudieren, meist bin ich an diesen Projekten mit beteiligt.</p>
         <p>Das Handwerk ist mir sehr wichtig, allerdings finde ich, das Arrangement muss zu den Menschen passen, die es spielen werden.</p>
         <p>Frage mich gerne an für ein Auftragsarrangement.</p>
